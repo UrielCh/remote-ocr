5af7be228575aaa67442b1b50ef13718877fc985
UrielCh <uriel.chemouni@gmail.com>
2022-01-17 17:38:37 +0200

    add some more files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { OcrService } from './ocr.service';
 
 @Injectable()
 export class AppService {
+  constructor(private ocr: OcrService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
---
+++ src/ocr.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { createWorker, Page, Worker } from 'tesseract.js';
+// import { Cat } from './interfaces/cat.interface';
+
+@Injectable()
+export class OcrService {
+  private _ocrWorkers: Worker[] = [];
+
+  private async getOcrWorker(): Promise<Worker> {
+    let worker = this._ocrWorkers.pop();
+    if (!worker) {
+      worker = createWorker({
+        // corePath: string
+        // langPath: string
+        // cachePath: string
+        // dataPath: string
+        // workerPath: string
+        // cacheMethod: string
+        // workerBlobURL: boolean
+        // gzip: boolean
+        // logger: (arg: any) => void,
+        // errorHandler: (arg: any) => void
+      });
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+    }
+    return worker;
+  }
+
+  private freeWorker(worker: Worker): void {
+    this._ocrWorkers.push(worker);
+  }
+
+  public async ocr(buffer: Buffer): Promise<Page> {
+    const worker = await this.getOcrWorker();
+    try {
+      const result = await worker.recognize(buffer);
+      return result.data;
+    } catch (error) {
+      throw error;
+      // console.log(error);
+    } finally {
+      this.freeWorker(worker);
+    }
+  }
+
+  // private readonly cats: Cat[] = [];
+}
